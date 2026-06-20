3e68f0f00b5485c9efe3c74f9d890a9ae916f3c0
rahul <rahulrajesh1399@gmail.com>
2026-06-20 21:43:08 +0530

    updated client url with render url

--- server/main.py
+++ server/main.py
@@ -20,7 +20,7 @@ async def lifespan(app:FastAPI):
 app = FastAPI(lifespan=lifespan)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["https://test-authlib-client.onrender.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
